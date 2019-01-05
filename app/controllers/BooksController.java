785c858b04361685b720d66eba980289cabd1105
Gennadii Genin <genin.gennadiigendos2010@yandex.ru>
2019-01-05 20:15:35 +0100

    Inited bookStore controller

---
+++ app/controllers/BooksController.java
@@ -0,0 +1,42 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+public class BooksController extends Controller {
+
+    // Show all books
+    public Result index() {
+        return TODO;
+    }
+
+    // Create book
+    public Result create() {
+        return TODO;
+    }
+
+    // Save book
+    public Result save() {
+        return TODO;
+    }
+
+    // Edit book
+    public Result edit(Integer id) {
+        return TODO;
+    }
+
+    // Update book
+    public Result update() {
+        return TODO;
+    }
+
+    // Delete book
+    public Result destroy(Integer id) {
+        return TODO;
+    }
+
+    // Display single book
+    public Result show(Integer id){
+        return TODO;
+    }
+}
