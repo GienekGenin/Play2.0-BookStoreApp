7e2849f6fd1d976ca00c99876d2a732afaaebab1
Gennadii Genin <genin.gennadiigendos2010@yandex.ru>
2019-01-06 14:28:40 +0100

    Implemented book deletion method

--- app/controllers/BooksController.java
+++ app/controllers/BooksController.java
@@ -75,7 +75,12 @@ public class BooksController extends Controller {
 
     // Delete book
     public Result destroy(Integer id) {
-        return TODO;
+        Book book = Book.findById(id);
+        if(book == null){
+            return notFound("Book not found");
+        }
+        Book.remove(book);
+        return redirect(routes.BooksController.index());
     }
 
     // Display single book
